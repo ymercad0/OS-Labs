26e455c51b006e77da8f9a0e7a25127464f7140c
Mercrist <yariel.then@gmail.com>
2023-02-03 12:20:13 -0400

    Completed fork() problem

--- Lab2/ex9.c
+++ Lab2/ex9.c
@@ -1,35 +1,36 @@
 /* Example of use of fork system call */
-#include <stdio.h>
-#include <unistd.h>
+#include <stdio.h>    // Prints
+#include <unistd.h>   // Forks
+#include <stdlib.h>   // Provides declaration for 'exit'
+#include <sys/wait.h> // Used for holding processes
 
 int main()
 {
     int pid;
-    pid = fork();
+    int status;    // wait(int *status_ptr) simply stores status information here
+    pid = fork();  // Parent returns result to child and child returns PID to parent
 
     if (pid < 0) {
         fprintf(stderr, "Fork failed!\n");
         exit(-1);
     }
 
-    // Child succesfully created
     else{
-        // Parent will always have the child's PID returned to it
-        printf("Creation of child process with the following PID: %d\n", pid);
-
+        // Switched to newly created child process
         if (pid==0) {
-            execlp("/bin/ps", "ps", NULL);
-            printf("Still around...\n");
             // Get relevant PID info
-            int my_pid = getpid(), ppid = getppid();
-            printf("I am the child, my process ID is: %d\n", my_pid);
-            printf("My parent's PID is: %d\n", ppid);
+            printf("I am the child, my PID is: %d\n", getpid());
+            printf("My parent's PID is: %d\n", getppid());
+            // execlp replaces the current process image, meaning anything after won't be executed.
+            // It won't return to the calling process.
+            printf("Still around...\n");
+            execlp("/bin/ps", "ps", NULL);
             exit(0);
         }
 
         // Switch over to the parent process
-        wait(&pid); // Wait for child process to end before exiting
-        exit(0); // exit whatever process is currently running
+        printf("I am the parent, the child's PID is: %d\n", pid);
+        wait(&status); // Wait for child process to end before exiting
+        exit(0); // exit the parent process after the child has ended
     }
-
 }
